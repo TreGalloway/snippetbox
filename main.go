ec7eb5220df023971f901b3e81da19f757082f1a
Tre Galloway <88118603+TreGalloway@users.noreply.github.com>
2024-07-18 22:42:14 -0500

    restarting the book

--- main.go
+++ main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hello from Snippetbox"))
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request ) {
-  id, err:= strconv.Atoi(r.PathValue("id"))
-  if err != nil || id < 1 {
-    http.NotFound(w,r)
-    return
-  }
-  msg := fmt.Sprintf("Display specific snippet with ID %d", id)
-  w.Write([]byte(msg))
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request){
-  w.Write([]byte("Display a form for creating a new snippet..."))
-}
 func main() {
-  mux := http.NewServeMux()
-  mux.HandleFunc("/{$}", home)
-  mux.HandleFunc("/snippet/view/{id}", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
-  log.Println("Starting server on :4000")
-  err := http.ListenAndServe(":4000", mux)
-  log.Fatal(err)
+	mux := http.NewServerMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
