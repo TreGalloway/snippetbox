5cc916efd28b0568e6b249142bf8671c6a7226ae
Tre Galloway <tregallowayofficial@gmail.com>
2025-07-12 11:56:16 -0500

    chapter 2.4 lets go

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Displaying snippet %d", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create a new snippet"))
+}
+
 func main() {
-	mux := http.NewServerMux()
-	mux.HandleFunc("/", home)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
